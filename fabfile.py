bf398938ae1d941f3f6a5726998bb27214d1b643
Martin Brochhaus <mbrochh@gmail.com>
2013-07-03 21:36:18 +0800

    Added a settings file to centralize some variables

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,20 @@
 from fabric.api import local
 from jinja2 import Environment, FileSystemLoader
 
+import settings
+
 
 template_env = Environment(loader=FileSystemLoader('.'))
 
 
 def build():
+    context = {}
+    for attr in dir(settings):
+        if not attr.startswith('__'):
+            context[attr] = getattr(settings, attr)
+
     template = template_env.get_template('source/index.html')
-    rendered_template = template.render()
+    rendered_template = template.render(**context)
     with open('presentation/index.html', 'wb') as fh:
         fh.write(rendered_template)
 
---
+++ settings.py
@@ -0,0 +1,9 @@
+PART1_COLOR = '#9d6c5c'
+PART2_COLOR = '#577983'
+PART3_COLOR = '#56815e'
+PART4_COLOR = '#017794'
+
+PART1_TITLE = 'Part I: Essential Linux Commands'
+PART2_TITLE = 'Part II: Editors & Productivity'
+PART3_TITLE = 'Part III: Getting Ready for Software Development'
+PART4_TITLE = 'Part IV: Advanced Tips & Tricks'
